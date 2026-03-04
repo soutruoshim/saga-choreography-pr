adf7a59f5e86f176d6a741907c0d86bdfbcd60d7
srhdp <srhdp@BIT-0157.local>
2026-03-04 10:26:06 +0700

    init project

---
+++ src/test/java/com/srhdp/sagachoreography/SagaChoreographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.sagachoreography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
