ea8273808d7119024d3733dc2113f558c6973723
srhdp <srhdp@BIT-0157.local>
2026-03-06 15:19:54 +0700

    common exception

---
+++ choreography-common/src/main/java/com/srhdp/choreographycommon/common/util/Record.java
@@ -0,0 +1,9 @@
+package com.srhdp.choreographycommon.common.util;
+
+import reactor.kafka.receiver.ReceiverOffset;
+
+public record Record<T>(String key,
+                        T message,
+                        ReceiverOffset acknowledgement) {
+}
+
